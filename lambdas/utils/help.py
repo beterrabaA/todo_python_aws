b9c0248fac2884b4273b03b97b64769a4f768556
Beterraba <ls6182315@gmail.com>
2024-07-04 16:06:29 -0300

    feat: add DecimalEncoder class for JSON serialization

---
+++ lambdas/utils/help.py
@@ -0,0 +1,11 @@
+import boto3
+from decimal import Decimal
+import json
+import os
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return str(obj)
+        return json.JSONEncoder.default(self, obj)
